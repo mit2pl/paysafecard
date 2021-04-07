df66139e429f1b810719b967a0b78fa77791e595
mit2 <mit2pl@gmail.com>
2021-04-07 14:44:23 +0200

    1.0

--- src/Payments/Pscpayments.php
+++ src/Payments/Pscpayments.php
@@ -10,11 +10,11 @@ Class PscpaymentClass {
     public function getlink() {
         //in this function we get link to website
         if(Config::get('paysafecard.psc_mode') == 'live') {
-            return "https://apitest.paysafecard.com/v1/payments/";
-        }else if(Config::get('paysafecard.psc_mode') == 'test') {
             return "https://api.paysafecard.com/v1/payments/";
+        }else if(Config::get('paysafecard.psc_mode') == 'test') {
+            return "https://apitest.paysafecard.com/v1/payments/";
         }else {
-            return "error";
+            Log::debug('An informational message.');
         }
     }
 }
