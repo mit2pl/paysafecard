8538d42694952117c9df8fa504b6c1e9a843555b
mit2 <mit2pl@gmail.com>
2021-04-22 15:48:03 +0200

    1.0

--- src/Config/Payment.php
+++ src/Config/Payment.php
@@ -11,15 +11,20 @@ return [
         test = to testing your code
         live = working live
     */
-    'psc_mode'    => env('PSC_MODE', 'live'),
+    'psc_mode' => env('PSC_MODE', 'live'),
     /*
         Your Paysafecard key
     */
-    'psc_key'   => env("PSC_KEY", ''),
+    'psc_key' => env("PSC_KEY", ''),
     /*
         You can enable error logs:
         1 - Enable
         0 - Disable
     */
-    'psc_logs'     => env('PSC_LOGGED', '1'),
+    'psc_logs' => env('PSC_LOGGED', '1'),
+
+    /*
+        id of shop
+    */
+    'psc_shopid' => env('PSC_SHOPID', ''),
 ];
--- src/Payments/Payment.php
+++ src/Payments/Payment.php
@@ -7,6 +7,7 @@
 namespace Mit2\Paysafecard\Payments;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 Class Payment {
 
@@ -27,11 +28,59 @@ Class Payment {
     }
 
     //create payment
-    public function create(array $createvalue) {
+    public function create(Request $getrequest, array $createvalue) {
         if($this->currency($createvalue['currency']) === true)
         {
             if($this->amount($createvalue['amount']) === true) {
-                echo "udalo sies";
+
+                //change amount from , to .
+                $amonts = str_replace(',', '.', $createvalue['amount']);
+
+                //add customer information
+                $customersinfo = array(
+                    "id" => $createvalue['customer_mail'],
+                    "ip" => $getrequest->ip(),
+                );
+
+                // check if you choose age and push
+                if($createvalue['minimumage'] != "") {
+                    array_push($customersinfo, "min_age", $createvalue['minimumage']);
+                }
+
+                // check if keylevel is exist
+                if($createvalue['kyclevel'] != "") {
+                    if($this->checkkyc($createvalue['kyclevel'] === true))
+                    {
+                        array_push($customersinfo, "kyc_level", $createvalue['kyclevel']);
+                    }
+                }
+                // put all information
+                $fullinformation = array(
+                    "currency" => $createvalue['currency'],
+                    "amount" => $amonts,
+                    "customer" => $customersinfo,
+                    "redirect" => array(
+                        "success_url" => $createvalue['success_url'],
+                        "failure_url" => $createvalue['failed_url'],
+                    ),
+                    "type" => "PAYSAFECARD",
+                    "notification_url" => $createvalue['notification_url'],
+                    "shop_id" => Config("paysafecard.psc_shopid"),
+
+                );
+                //check if submerchant id exist
+                if($createvalue['submerchantid'] != "") {
+                    array_push($fullinformation, "submerchant_id", $createvalue['submerchantid']);
+                }
+
+                if($createvalue['correlationid'] != "")
+                {
+                    $head = ["Correlation-ID: ". $createvalue['correlationid']];
+                } else {
+                    $head = [];
+                }
+
+                $this->getcurl("afd", "POST", $head);
             }else {
                 abort(404);
             }
@@ -42,8 +91,57 @@ Class Payment {
     }
 
     //get curl information
-    private function getcurl() {
+    private function getcurl($parameterrequire, $method) {
         $startcurl = curl_init();
+
+        $keysend = array(
+            "Authorization: Basic " . base64_encode(Config("paysafecard.psc_key")),
+            "Content-Type: application/json",
+        );
+        //add key to curl
+        curl_setopt($startcurl, CURLOPT_HTTPHEADER, $keysend);
+
+        if($method == "POST") {
+            curl_setopt($startcurl, CURLOPT_URL, $this->getlink());
+            curl_setopt($startcurl, CURLOPT_POSTFIELDS, json_encode($parameterrequire));
+            curl_setopt($startcurl, CURLOPT_POST, true);
+        } elseif($method == "GET") {
+            if(!empty($parameterrequire)) {
+                curl_setopt($startcurl, CURLOPT_URL, $this->getlink() . $parameterrequire);
+                curl_setopt($startcurl, CURLOPT_POST, false);
+            } else {
+                curl_setopt($startcurl, CURLOPT_URL, $this->getlink());
+            }
+        } else {
+            //text ze jest wybrana zla forma
+            if(Config('paysafecard.psc_logs') == '1') {
+                Log::debug('Connection mode is wrong');
+            }
+            abort(404);
+        }
+        curl_setopt($startcurl, CURLOPT_PORT, 443);
+        curl_setopt($startcurl, CURLOPT_TIMEOUT, 60);
+        curl_setopt($startcurl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($startcurl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($startcurl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($startcurl, CURLOPT_HEADER, false);
+
+        if(is_array($parameterrequire)) {
+            $parameterrequire['request_url'] = $this->getlink();
+        } else {
+            $geturl              = $this->getlink() . $parameterrequire;
+            $parameterrequire                = array();
+            $parameterrequire['request_url'] = $geturl;
+        }
+        $this->request  = $parameterrequire;
+        $this->response = json_decode(curl_exec($startcurl), true);
+
+        $this->curl["info"]        = curl_getinfo($startcurl);
+        $this->curl["error_nr"]    = curl_errno($startcurl);
+        $this->curl["error_text"]  = curl_error($startcurl);
+        $this->curl["http_status"] = curl_getinfo($startcurl, CURLINFO_HTTP_CODE);
+        curl_close($startcurl);
+        $this->getlink();
     }
     
     //check if currency is ok
@@ -66,6 +164,18 @@ Class Payment {
         }else {
             if(Config('paysafecard.psc_logs') == '1') {
                 Log::debug('Amount is not numeric');
+            }
+            return false;
+        }
+    }
+
+    //check if kyc level is SIMPLE or FULL
+    public function checkkyc($kycvalue) {
+        if($kycvalue == "SIMPLE" OR $kycvalue == "FULL") {
+            return true;
+        }else {
+            if(Config('paysafecard.psc_logs') == '1') {
+                Log::debug('You choose wrond key level, must be SIMPLE or FULL');
             }
             return false;
         }
